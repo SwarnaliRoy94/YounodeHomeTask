25f7937e2dc3907de38ebcbc0808c1efa9296ebe
Swarnali Roy <swarnali.roy093@gmail.com>
2023-11-22 14:17:34 +0600

    added necessary packages for navigation

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -6,7 +6,7 @@ const Stack = createStackNavigator();
 
 const Root = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   );
