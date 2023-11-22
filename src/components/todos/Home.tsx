7c8b5b14d9fac51a96ea2f2af479b6822ae9b059
Swarnali Roy <swarnali.roy093@gmail.com>
2023-11-22 12:38:30 +0600

    added home screen

---
+++ src/components/todos/Home.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react';
+import {Text, View} from 'react-native';
+
+const Home: FC = () => {
+  return (
+    <View>
+      <Text>This is home!</Text>
+    </View>
+  );
+};
+
+export default Home;
---
+++ src/navigation/Router.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../components/todos/Home';
+
+const Stack = createStackNavigator();
+
+const Root = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+};
+
+export default Root;
