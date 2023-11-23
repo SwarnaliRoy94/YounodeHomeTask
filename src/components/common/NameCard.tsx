5b21691ed3e86e2de43eb9b6a4021213386587be
Swarnali Roy <swarnali.roy093@gmail.com>
2023-11-24 00:34:25 +0600

    modified nameCard

--- src/components/common/NameCard.tsx
+++ src/components/common/NameCard.tsx
@@ -8,29 +8,26 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
   },
   text: {
-    fontSize: 16,
+    fontSize: 20,
   },
   nameText: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: '600',
+    color: '#A504BF',
   },
   taskNum: {
     fontSize: 16,
-    fontWeight: '600',
     color: '#884EA0',
   },
 });
 
-const NameCard: FC<NameCardProps> = ({userName, noOfTasks}) => {
+const NameCard: FC<NameCardProps> = ({userName}) => {
   return (
     <View style={styles.wrapper}>
       <Text style={styles.text}>
-        Hello, <Text style={styles.nameText}>{userName} </Text>!
+        Hello, <Text style={styles.nameText}>{userName}</Text>!
       </Text>
-      <Text style={styles.text}>
-        You have <Text style={styles.taskNum}>{noOfTasks} new tasks </Text>
-        reamining!
-      </Text>
+      <Text style={styles.taskNum}>You have some new tasks to finish!! </Text>
     </View>
   );
 };
